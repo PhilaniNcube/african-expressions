ba816219d17c4267a2c387c59c529dccdf20973b
Philani Ncube <ncbphi001@gmail.com>
2022-08-11 09:24:29 +0200

    hero image

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,48 +1,58 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { Fragment } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/future/image';
 
 const Hero = () => {
   const router = useRouter();
 
   return (
-    <div className="min-h-[75vh] bg-home-bg bg-center  bg-blend-overlay bg-cover bg-no-repeat px-6 md:px-4">
-      <div className="min-h-[60vh] my-auto max-w-7xl mx-auto py-12 flex flex-col items-center justify-center">
-        <h1 className="font-georgiaBold text-deep text-center text-2xl md:text-4xl lg:text-6xl">
-          A premium  <br /> twist
-        </h1>
-        <p className="text-deep text-center text-md md:text-lg w-full font-bold mt-4">
-          Perfectly blended <br /> for any design.
-        </p>
+    <div className="min-h-[75vh] bg-home-bg bg-center  bg-blend-overlay bg-cover bg-no-repeat relative">
+      <Image
+        src="/images/hero.jpeg"
+        alt="hero"
+        className="aspect-video object-cover h-full w-full"
+        height={6000}
+        width={4000}
+      />
+      <div className="absolute inset-0">
+        <div className="min-h-[60vh] my-auto max-w-7xl mx-auto py-12 flex flex-col items-center justify-center">
+          <h1 className="font-georgiaBold text-deep text-center text-2xl md:text-4xl lg:text-6xl">
+            A premium <br /> twist
+          </h1>
+          <p className="text-deep text-center text-md md:text-lg w-full font-bold mt-4">
+            Perfectly blended <br /> for any design.
+          </p>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 mt-12 gap-12">
-          <span
-            onClick={() => {
-              router.push('/yarns');
-            }}
-            className="h-64 w-64 bg-accent/60 hover:bg-accent shadow-xl transition-colors duration-150 ease-out cursor-pointer hover:shadow-md rounded-lg text-white flex flex-col justify-center items-center"
-          >
-            <img src="/images/Icons-03.svg" alt="" className="h-24 w-24" />
-            <p className="text-xl">Browse Our Yarns</p>
-          </span>
-          <span
-            onClick={() => {
-              router.push('/patterns');
-            }}
-            className="h-64 w-64 bg-accent/60 hover:bg-accent shadow-xl transition-colors duration-150 ease-out cursor-pointer hover:shadow-md rounded-lg text-white flex flex-col justify-center items-center"
-          >
-            <img src="/images/Icons-02.svg" alt="" className="h-24 w-24" />
-            <p className="text-xl">Browse Patterns</p>
-          </span>
-          <span
-            onClick={() => {
-              router.push('/tutorials');
-            }}
-            className="h-64 w-64 bg-accent/60 hover:bg-accent shadow-xl transition-colors duration-150 ease-out cursor-pointer hover:shadow-md rounded-lg text-white flex flex-col justify-center items-center"
-          >
-            <img src="/images/Icons-01.svg" alt="" className="h-24 w-24" />
-            <p className="text-xl">Tutorials</p>
-          </span>
+          <div className="grid grid-cols-1 lg:grid-cols-3 mt-12 gap-12">
+            <span
+              onClick={() => {
+                router.push("/yarns");
+              }}
+              className="h-64 w-64 bg-accent/60 hover:bg-accent shadow-xl transition-colors duration-150 ease-out cursor-pointer hover:shadow-md rounded-lg text-white flex flex-col justify-center items-center"
+            >
+              <img src="/images/Icons-03.svg" alt="" className="h-24 w-24" />
+              <p className="text-xl">Browse Our Yarns</p>
+            </span>
+            <span
+              onClick={() => {
+                router.push("/patterns");
+              }}
+              className="h-64 w-64 bg-accent/60 hover:bg-accent shadow-xl transition-colors duration-150 ease-out cursor-pointer hover:shadow-md rounded-lg text-white flex flex-col justify-center items-center"
+            >
+              <img src="/images/Icons-02.svg" alt="" className="h-24 w-24" />
+              <p className="text-xl">Browse Patterns</p>
+            </span>
+            <span
+              onClick={() => {
+                router.push("/tutorials");
+              }}
+              className="h-64 w-64 bg-accent/60 hover:bg-accent shadow-xl transition-colors duration-150 ease-out cursor-pointer hover:shadow-md rounded-lg text-white flex flex-col justify-center items-center"
+            >
+              <img src="/images/Icons-01.svg" alt="" className="h-24 w-24" />
+              <p className="text-xl">Tutorials</p>
+            </span>
+          </div>
         </div>
       </div>
     </div>
